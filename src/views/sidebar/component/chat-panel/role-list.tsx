c33126c85b4b0d99426a59e56deb8897c694ce15
yoran <295563358@qq.com>
2025-12-18 00:27:40 +0800

    Fix onClick type from MouseEvent to React.MouseEvent

---
+++ src/views/sidebar/component/chat-panel/role-list.tsx
@@ -0,0 +1,73 @@
+import {
+  PersonIcon,
+  EditIcon,
+  AddSmallIcon,
+  CloseIcon,
+} from "../../../../ui/icon";
+import styles from "./css/styles.module.css";
+
+import type { RoleItem } from "../../../../core/storage/role-storage";
+
+export type RoleListProps = {
+  roles: RoleItem[];
+  selectedRole: RoleItem | null;
+  addRole: () => void;
+  editRole: (role: RoleItem) => Promise<void>;
+  deleteRole: (role: RoleItem) => Promise<void>;
+  selectRole: (role: RoleItem) => Promise<void>;
+};
+
+export const RoleList = ({
+  roles,
+  selectedRole,
+  addRole,
+  editRole,
+  deleteRole,
+  selectRole,
+}: RoleListProps) => {
+  const roleItem = (role: RoleItem, index: number) => {
+    const isActive = role.name === selectedRole?.name;
+
+    return (
+      <div
+        className={`${styles.historyFoldItem} ${
+          isActive ? styles.historyFoldItemActive : ""
+        }`}
+        key={index}
+        onClick={() => selectRole(role)}
+      >
+        <PersonIcon />
+        <div className={styles.historyFoldText}>{role.name}</div>
+        <div className={styles.historyFoldActions}>
+          <EditIcon
+            onClick={(e?: React.MouseEvent) => {
+              e?.stopPropagation();
+              editRole(role);
+            }}
+          />
+          <CloseIcon
+            onClick={async (e?: React.MouseEvent) => {
+              e?.stopPropagation();
+              deleteRole(role);
+            }}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.historyFoldList}>
+      {roles.map((role: RoleItem, index: number) => roleItem(role, index))}
+      <div
+        className={styles.historyFoldItem}
+        onClick={() => {
+          addRole();
+        }}
+      >
+        <AddSmallIcon />
+        <div className={styles.historyFoldText}>新增角色</div>
+      </div>
+    </div>
+  );
+};
